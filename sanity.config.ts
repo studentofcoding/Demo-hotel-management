139e115e9df26e73d70195004ce141b70e286672
Shyaminda <shyamindaSenevirathna@gmail.com>
2023-12-17 18:27:49 +0530

    add sanity IO cms

---
+++ sanity.config.ts
@@ -0,0 +1,20 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'hotel-management',
+
+  projectId: '577y22v2',
+  dataset: 'production',
+
+  basePath: '/studio',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
