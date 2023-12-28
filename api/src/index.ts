2e48dc26b11c69216eed2e1a4c84399d30932f96
Shyaminda <shyamindaSenevirathna@gmail.com>
2023-12-28 22:44:24 +0530

    add login functionality

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,7 +2,8 @@ import express,{Request,Response} from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
-import userRoute from './routes/users';
+import userRouter from './routes/users';
+import authRouter from './routes/auth';
 
 mongoose.connect(process.env.MONGO as string)
 .then(() => console.log('Connected to Database successfully'))
@@ -13,7 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/api/users",userRoute);  //this route is used to register the user
+app.use("/api/users",userRouter);  //this route is used to register the user
+app.use("/api/auth",authRouter);  //this route is used to login the user
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
---
+++ api/src/routes/auth.ts
@@ -0,0 +1,57 @@
+import express, { Request, Response } from "express";
+import { check, validationResult } from "express-validator";
+import bcrypt from "bcryptjs";
+import User from "../models/user";
+import jwt from "jsonwebtoken";
+
+const authRouter = express.Router();
+
+authRouter.post(    "/login",
+  [
+    check("email", "Email is required").isEmail(),
+    check("password", "Password is required atleast 6 characters").isLength({
+      min: 6,
+    }),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).send({ message: "Invalid Credentials" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).send({ message: "Invalid Credentials" });
+      }
+
+      const token = jwt.sign(   //creating an access token  If the passwords match, it generates a JSON Web Token (JWT) using jwt.sign containing the userId from the found user. This token is signed with a secret key 
+        {
+          userId: user.id,
+        },
+        process.env.JWT_SECRET_KEY as string,
+        {
+          expiresIn: "1d",
+        }
+      );
+         res.cookie("auth_token", token, {      //The generated token is then set in a cookie named "auth_token" with options for HTTP-only access, secure transmission in production, and an expiration time of 24 hours 
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 86400000,
+        });
+
+        res.status(200).json({ message: "Logged in successfully", userId: user.id});
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ message: "Error logging in" });
+    }
+  }
+);
+
+export default authRouter;
