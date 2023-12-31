96dc716789e7ed6588d6cf58e375720b2e69ad4a
Shyaminda <shyamindaSenevirathna@gmail.com>
2023-12-31 23:54:57 +0530

    setup cloudinary

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,6 +6,24 @@ import userRouter from './routes/users';
 import authRouter from './routes/auth';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import { v2 as cloudinary } from 'cloudinary';
+
+// cloudinary.config({ 
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+//     api_key: process.env.CLOUDINARY_API_KEY, 
+//     api_secret: process.env.CLOUDINARY_API_SECRET 
+// });
+
+try {
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+    console.log('Connected to Cloudinary successfully');
+} catch (error) {
+    console.log(error);
+};
 
 mongoose.connect(process.env.MONGO as string)
 .then(() => console.log('Connected to Database successfully'))
@@ -20,7 +38,7 @@ app.use(cors({
     credentials: true,      //this is used to send the cookie to the server so that the server can identify the user where the security is increased
 }));
 
-app.use(express.static(path.join(__dirname, "../../client/dist")))
+app.use(express.static(path.join(__dirname, "../../client/dist")));  //make the connection to the frontend to run in the same localhost or the same server simultaneously
 
 app.use("/api/users",userRouter);  //this route is used to register the user
 app.use("/api/auth",authRouter);  //this route is used to login the user
