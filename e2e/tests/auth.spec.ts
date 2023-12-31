d583524a1e9e3cee1b30081469fd7689a3919ce4
Shyaminda <shyamindaSenevirathna@gmail.com>
2023-12-31 15:19:16 +0530

    test the login functionality by testing

---
+++ e2e/tests/auth.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+const UI_URL = 'http://localhost:5173/';
+
+test('should allow the user to login', async ({ page }) => {
+  await page.goto(UI_URL);       //here what basically going to happen is that we are going to visit the UI_URL which is the url of the client
+
+  //get the sign in button
+  await page.getByRole("link",{name:"Sign In"}).click();  //here we are getting the sign in button by role and name
+
+  await expect(page.getByRole("heading",{name:"Sign In"})).toBeVisible();  //here we are checking if the heading is visible or not
+
+  await page.locator("[name='email']").fill("chamika@gmail.com");  //here we are filling the email field with the email
+  await page.locator("[name='password']").fill("chamika1234");  //here we are filling the password field with the password
+
+  await page.getByRole("button",{name:"Login"}).click();  //here we are clicking the sign in button
+
+  await expect(page.getByText("Login Successful")).toBeVisible();  //here we are checking if the text is visible or not
+  await expect(page.getByRole("link",{name:"My Bookings"})).toBeVisible();  //here we are checking if the link is visible or not
+  await expect(page.getByRole("link",{name:"My Hotels"})).toBeVisible();  //here we are checking if the link is visible or not
+  await expect(page.getByRole("button",{name:"Sign Out"})).toBeVisible();  //here we are checking if the link is visible or not
+});
+
+
+
