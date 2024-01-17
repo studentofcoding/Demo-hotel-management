4a94d07b191fd4a36cf4581385769022622146de
Shyaminda <shyamindaSenevirathna@gmail.com>
2024-01-17 16:22:24 +0530

    Add hotel detail page test case

--- e2e/tests/search-hotel.spec.ts
+++ e2e/tests/search-hotel.spec.ts
@@ -27,3 +27,14 @@ test("should show hotel search results", async ({ page }) => {
   await expect(page.getByText("Hotels found in colombo")).toBeVisible();
   await expect(page.getByText("kingsbury").first()).toBeVisible();
 });
+
+test("should show hotel detail", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByPlaceholder("Where are you going?").fill("colombo 1");
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await page.getByText("kingsbury").click();
+  await expect(page).toHaveURL(/detail/);
+  await expect(page.getByRole("button", { name: "Book now" })).toBeVisible();
+});
