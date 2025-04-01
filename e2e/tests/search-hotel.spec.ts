ed5b70552aad2ac7a82d3122c45c1281dc58f789
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2025-04-02 00:20:18 +0530

    Update database connection strings and modify test credentials

--- e2e/tests/search-hotel.spec.ts
+++ e2e/tests/search-hotel.spec.ts
@@ -10,8 +10,8 @@ test.beforeEach(async ({ page }) => {
 
   await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
 
-  await page.locator("[name=email]").fill("hima@gmail.com");
-  await page.locator("[name=password]").fill("hima1234");
+  await page.locator("[name=email]").fill("shyaminda7257@gmail.com");
+  await page.locator("[name=password]").fill("12345678");
 
   await page.getByRole("button", { name: "Login" }).click();
 
